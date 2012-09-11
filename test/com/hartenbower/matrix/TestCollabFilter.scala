8404337ca0d56e708f9d18e7fb773b7d5a7346d2
rh <reid@localhost.localdomain>
2012-09-11 12:19:19 -0700

    reconciled with octave version

--- test/com/hartenbower/matrix/TestCollabFilter.scala
+++ test/com/hartenbower/matrix/TestCollabFilter.scala
@@ -12,14 +12,23 @@ object TestCollabFilter {
     // import com.hartenbower.matrix._; import LogisticRegression._ ; import NeuralNet._; import Util._;import java.io._; import Util.Io.RichFile.enrichFile
     val f = Io.parseOctaveDataFile("ex8_movies.txt")
     val ff = Io.parseOctaveDataFile("ex8_movieParams.txt")
-    val x = ff.get("X").get.asInstanceOf[MatrixD]
-    val y = f.get("Y").get.asInstanceOf[MatrixD]
-    val theta = ff.get("Theta").get.asInstanceOf[MatrixD]
+    val x = ff.get("X").get.asInstanceOf[MatrixD] //  sampls x features
+    val y = f.get("Y").get.asInstanceOf[MatrixD] // sampls x users
+    val theta = ff.get("Theta").get.asInstanceOf[MatrixD]  // users x features
+    val num_users = 4; val num_movies = 5; val num_features = 3;
     val (m, n) = x.dims()
-    val r = f.get("R").get.asInstanceOf[MatrixD]
+    val smallX = x.rowSubset( Range(1,num_movies+1).toArray).columnSubset(Range(1,num_features+1).toArray)
+    val smallY = y.rowSubset( Range(1,num_movies+1).toArray).columnSubset(Range(1,num_users+1).toArray)
+    val smallTh = theta.rowSubset( Range(1,num_users+1).toArray).columnSubset(Range(1,num_features+1).toArray)
+    val r = f.get("R").get.asInstanceOf[MatrixD] // sampls x users
+    val smallR =  r.rowSubset( Range(1,num_movies+1).toArray).columnSubset(Range(1,num_users+1).toArray)
     var lambda = 0d
     var j = CollaborativeFiltering.cost(x, theta, y, r, lambda)
+    var smallJ = CollaborativeFiltering.cost(smallX, smallTh, smallY, smallR, lambda)
+    assert(Util.Math.aboutEq(smallJ, 22.22,.005))
     lambda = 1.5
     j = CollaborativeFiltering.cost(x, theta, y, r, lambda)
-  }
+    smallJ = CollaborativeFiltering.cost(smallX, smallTh, smallY, smallR, lambda)
+    assert(Util.Math.aboutEq(smallJ, 31.34,.005))
+   }
 }
